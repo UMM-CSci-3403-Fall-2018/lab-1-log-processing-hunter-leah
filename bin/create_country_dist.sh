5c3d9aaf14ae43bf9137623801c028542c711bc7
welch298 <welch298@morris.umn.edu>
2018-09-11 09:22:56 -0500

    Update files for easier viewing

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -4,6 +4,10 @@ cat html_components/country_dist_header.html >> $1/country_dist.html
 
 find $1 -name "*failed_login_data*" -exec awk '{print $5}' {} + | sort | cat >> $1/sorted_ips.txt
 
-join --nocheck-order etc/country_IP_map.txt $1/sorted_ips.txt | awk '{print $2}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | cat >> $1/country_dist.html
+join --nocheck-order etc/country_IP_map.txt $1/sorted_ips.txt |
+awk '{print $2}' | sort | uniq -c |
+awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | cat >> $1/country_dist.html
 
 rm $1/sorted_ips.txt
+
+cat html_components/country_dist_footer.html >> $1/country_dist.html
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,7 +3,7 @@
 cd $1
 
 grep -wr "Failed password for" --exclude=failed_login_data.txt |
-awk '{if($9 == "invalid"){print $1, $2, substr($3, 1, 2), $11, $13}
-else{print $1, $2, substr($3, 1, 2), $9, $11}}' |
+awk '{if($9 == "invalid"){print $1, $2, substr($3, 1, 2), $11, $13}else{print $1, $2, substr($3, 1, 2), $9, $11}}' |
 awk -F: '{print $2}' |
 cat >> failed_login_data.txt
+
