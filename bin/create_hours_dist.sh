5d63b0c6c3c1f7c261c7e9ab4e434727506949ee
welch298 <welch298@morris.umn.edu>
2018-09-10 19:59:06 -0500

    Add assemble_report.sh, create_country_dist.sh, create_hours_dist.sh, creat_username_dist.sh, process_client_logs.sh, and process_logs.sh

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+find $1 -name "*failed_login_data*" -exec awk '{print $3}' {} + | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | cat >> $1/hours_content.html
+
+bin/wrap_contents.sh $1/hours_content.html html_components/hours_dist $1/hours_dist.html
